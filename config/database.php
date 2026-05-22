1032ad501af7dadeba5db1d5dc6fac72c27d39e5
linotrnasasgwapo <linotrnasasgwapo@gmail.com>
2026-05-22 11:06:37 +0800

    fix mysql pdo

--- config/database.php
+++ config/database.php
@@ -7,13 +7,20 @@
 define('DB_HOST', 'kodama.proxy.rlwy.net');
 define('DB_NAME', 'railway');
 define('DB_USER', 'root');
-define('DB_PASS', ' igyIIECWHWvLJVpXSNPIUCoKhqaiYhpW');
+define('DB_PASS', 'igyIIECWHWvLJVpXSNPIUCoKhqaiYhpW');
 define('DB_PORT', '50255');
+
 try {
+
     $pdo = new PDO(
-        "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4",
+        "mysql:host=" . DB_HOST .
+        ";port=" . DB_PORT .
+        ";dbname=" . DB_NAME .
+        ";charset=utf8mb4",
+
         DB_USER,
         DB_PASS,
+
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -22,6 +29,8 @@ try {
     );
 
 } catch (PDOException $e) {
+
     die("Database connection failed: " . $e->getMessage());
+
 }
 ?>
